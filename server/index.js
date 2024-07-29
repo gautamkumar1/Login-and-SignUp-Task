fa474b1520a98050876f0da2cc652c72710b943f
gautamkumar1 <gautamraaz936@gmail.com>
2024-07-29 14:12:08 +0530

    added frontend url

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const connectDb = require('./utils/Db');
 const { register, login, getAllUsers } = require('./controllers/auth-controllers');
 const cors = require('cors')
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "https://task-1-login-and-singup.vercel.app",
   method: "GET,POST,PUT, DELETE, PATCH,HEAD",
   Credential: true,
 };
